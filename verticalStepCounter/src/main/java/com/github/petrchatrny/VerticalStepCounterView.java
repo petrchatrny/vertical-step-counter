b524a71385ff930d4df05f52758db1dbf158a977
Mr. Anderson <56938867+PRIONxd@users.noreply.github.com>
2020-08-30 00:59:50 +0200

    Scrolling fixed

--- verticalStepCounter/src/main/java/com/github/petrchatrny/VerticalStepCounterView.java
+++ verticalStepCounter/src/main/java/com/github/petrchatrny/VerticalStepCounterView.java
@@ -121,6 +121,13 @@ public class VerticalStepCounterView extends ListView {
         dispatchThawSelfOnly(container);
     }
 
+    // scroll solution
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+
     public void setNumberColor(int color) {
         numberColor = color;
     }
